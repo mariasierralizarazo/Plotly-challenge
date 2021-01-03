ce75e7adef5129e766d5bba1360a3011a565d5cc
MariaSierra <66576032+mariasierralizarazo@users.noreply.github.com>
2021-01-03 00:46:55 -0500

    Working code app.js

--- static/js/app.js
+++ static/js/app.js
@@ -13,15 +13,21 @@ function init() {
         });
 
         //Initialize with a sample
-        var sample = names[0];
-        bubbleChart(sample);
-
-
+        barChart(names[0]);
+        bubbleChart(names[0]);
+        panel_data(names[0]);
 
     })
 }
 
 init();
+// taken function from Index file
+function optionChanged(selected_id)
+{
+    barChart(selected_id);
+    bubbleChart(selected_id);
+    panel_data(selected_id);
+}
 
 function bubbleChart(selected_id) {
     d3.json("samples.json").then((data) => {
@@ -44,15 +50,54 @@ function bubbleChart(selected_id) {
             }
 
         ];
-        Ploty.newPlot("bubble", data, layout);
+        Plotly.newPlot("bubble", data, layout);
 
     });
-
-
 }
 
+function barChart(selected_id) {
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var samples_filter = samples.filter(x => x.id == selected_id);
 
+        var otu_id = samples_filter[0].otu_ids;
 
+        var y_data_10 = otu_id.slice(0,10).map(x => `OTU ${x}`).reverse();
+
+        var bardata = [
+            {
+                y: y_data_10,
+                x: samples_filter[0].sample_values.slice(0, 10).reverse(),
+                text: samples_filter[0].otu_labels.slice(0, 10).reverse(),
+                type: "bar",
+                orientation: "h",
+            }
+        ];
+
+        var layout = {
+            title: "Top 10 Bacteria Cultures",
+        }
+        Plotly.newPlot("bar", bardata, layout);
+
+    });
+}
+
+function panel_data(selected_id)
+{
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var samples_filter = samples.filter(x => x.id == selected_id);
+
+        // Selecting information place 
+        var information = d3.select("#sample-metadata");
+        information.html("");
+
+        Object.entries(samples_filter[0]).forEach(([key, value]) => {
+            information.append("h5").text(`${key}: ${value}`);
+        });
+    });
+
+}
 
 
 
